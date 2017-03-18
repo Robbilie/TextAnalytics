cfe6e0c66cd1a2c18160b96099f4ca8290a9c83a
Robert Schuh <github@eneticum.de>
2017-03-18 21:26:00 +0100

    more comparators

---
+++ src/main/java/de/unidue/langtech/teaching/pp/schuh/comparator/CountSentenceComparator.java
@@ -0,0 +1,11 @@
+package de.unidue.langtech.teaching.pp.schuh.comparator;
+
+public class CountSentenceComparator extends AbstractSentenceComparator {
+
+	@Override
+	public float compare() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/main/java/de/unidue/langtech/teaching/pp/schuh/comparator/LengthSentenceComparator.java
@@ -0,0 +1,11 @@
+package de.unidue.langtech.teaching.pp.schuh.comparator;
+
+public class LengthSentenceComparator extends AbstractSentenceComparator {
+
+	@Override
+	public float compare() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
