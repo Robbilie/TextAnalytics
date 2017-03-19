75da4ab6842791ef46a68ce7405b73e7b9026306
Robert Schuh <github@eneticum.de>
2017-03-19 18:51:59 +0100

    calc test

--- src/test/java/de/unidue/langtech/teaching/pp/schuh/FMeasureCalculatorTest.java
+++ src/test/java/de/unidue/langtech/teaching/pp/schuh/FMeasureCalculatorTest.java
@@ -1,12 +1,27 @@
 package de.unidue.langtech.teaching.pp.schuh;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class FMeasureCalculatorTest {
 	
 	@Test
 	public void testFMeasureComparator() {
+		FMeasureCalculator fmc = new FMeasureCalculator();
 		
+			List<Float> goldValues = new ArrayList<Float>();
+				goldValues.add(0f);
+			fmc.setGoldValues(goldValues);
+			
+			List<Float> testValues = new ArrayList<Float>();
+				testValues.add(0f);
+			fmc.setTestValues(testValues);
+			
+		assertEquals(fmc.calculateFMeasure(), 0f, 0f);
 	}
 
 }
